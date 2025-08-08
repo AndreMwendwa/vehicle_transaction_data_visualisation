6416fb37b535cc9ed3e2f4bc07cdaf7497428ee3
AndreMwendwa <mwendwakiko@gmail.com>
2025-08-08 13:36:29 -0400

    EV proportion map added

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,17 +15,25 @@ base_folder = (
 maps = [
     {
         "title": "Transactions",
-        "file": os.path.join(base_folder, "transaction_numbers_map.html")
+        "file": os.path.join(base_folder, "transaction_numbers_map.html"),
+        "text": "Map showing the number of transactions by FSA."
     },
     {
         "title": "Transactions per capita",
-        "file": os.path.join(base_folder, "transaction_numbers_per_capita_map.html")
+        "file": os.path.join(base_folder, "transaction_numbers_per_capita_map.html"),
+        "text": "Map showing the number of transactions per capita by FSA."
+    },
+    {
+        "title": "Proportion of EVs",
+        "file": os.path.join(base_folder, "ev_proportion_map.html"),
+        "text": "Map showing the proportion of electric vehicles by FSA. Figures are in percentages."
     }
 ]
 
 # Loop through and render each map
 for m in maps:
     st.subheader(m["title"])
+    st.write(m["text"])
     if not os.path.exists(m["file"]):
         st.error(f"Could not find `{m['file']}`. Make sure it's in the correct folder.")
     else:
